2998084d2165d031532f43e5232a2256c5557466
S4URABH <30949385+S4URABH@users.noreply.github.com>
2018-06-23 13:51:07 +0530

    initial commit

--- resources/scripts/script.js
+++ resources/scripts/script.js
@@ -1,12 +1,11 @@
 if('serviceWorker' in navigator){
-    window.addEventListener('load', function(){
-       navigator.serviceWorker.register('./serviceworker.js').then(function(){
-           console.log('SW Registered...');
-       },function(err){
-           console.log(err);
-       })
+    window.addEventListener('load',function(){
+        navigator.serviceWorker.register('./serviceworker.js').then(function(){
+            console.log('Service worker registered');
+        },function(err){
+            console.log(err);
+        })
     })
-}
-else{
-    console.log('nope')
+}else{
+    console.log("nope");
 }
